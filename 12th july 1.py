83bb69b1de379fbfceb5fe8794e278579ad0d7e2
sivaranjani koduru <kodurusivaranjani2001@gmail.com>
2025-09-10 18:27:16 +0530

    Add files via upload
    This Python script is a practice example for loops, sequences, and ranges.

---
+++ 12th july 1.py
@@ -0,0 +1,52 @@
+x=["sita",12,1.2]
+print (x)
+
+
+x=["sita",12,2.3]
+for i in x:
+    print(i)
+    
+    
+#x=21
+#for i in x: // not possible
+ #   pritn(i)
+
+#// syntax used for  i in is obtain collection or sequence it can be a list ,
+# string , tuple
+    
+x="sita"
+for i in x:
+    print(i)
+
+for i in ("sita"):
+    print(i)
+
+for i in ["sita",3]:
+    print(i)
+
+for i in("sita",3,2.4):
+         print(i)
+
+
+# always ascending order start to stop
+#slicing (start,stop,jump)
+for i in range(6):
+    print(i)
+
+for i in range(10,20,2):
+    print(i)
+
+for i in range(20,10,-1):
+    print(" stat",i)
+
+
+for i in range(1,10):
+    if(i%2!=0):
+        print(i)
+
+i=1
+while i<=3:
+    print("hii")
+    i=i+1
+         
+         
